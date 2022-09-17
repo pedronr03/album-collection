d42b389642b13389f489bcb7a7d41766cb25c040
Pedro Reis <pedronr0309@gmail.com>
2022-09-17 14:26:32 -0300

    feat: create mutations and set inputs

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,3 +1,13 @@
-const resolvers = {};
+import Album from "./album.resolver";
+import Mutation from "./mutation.resolver";
+import Query from "./query.resolver";
+import Song from "./song.resolver";
+
+const resolvers = {
+  Query,
+  Album,
+  Song,
+  Mutation
+};
 
 export default resolvers;
