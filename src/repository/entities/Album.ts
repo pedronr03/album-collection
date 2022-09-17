9960217bb2214a673c322bcfce3c7714f2084bb6
Pedro Reis <pedronr0309@gmail.com>
2022-09-17 14:54:00 -0300

    feat: create entities

---
+++ src/repository/entities/Album.ts
@@ -0,0 +1,7 @@
+export default interface Album {
+  id: number
+  title: string
+  releasedAt: Date
+  artistId: number
+  genreId: number
+}
---
+++ src/repository/entities/Song.ts
@@ -0,0 +1,6 @@
+export default interface Song {
+  id: number
+  title: string
+  length: number
+  albumId: number
+}
