74cec1f2575349ca14af585ad7976de0cd58a3f8
Pedro Reis <pedronr0309@gmail.com>
2022-09-17 19:41:10 -0300

    feat: implements album resolver

--- src/graphql/resolvers/album.resolver.ts
+++ src/graphql/resolvers/album.resolver.ts
@@ -1,3 +1,16 @@
-const Album = {};
+import ArtistRepository from "../../repository/artist.repository";
+import Album from "../../repository/entities/Album";
+import GenreRepository from "../../repository/genre.repository";
+
+const Album = {
+  artist: (root: Album) => {
+    const artistRepository = new ArtistRepository();
+    return artistRepository.getById(root.artistId);
+  },
+  genre: (root: Album) => {
+    const genreRepository = new GenreRepository();
+    return genreRepository.getById(root.genreId);
+  }
+};
 
 export default Album;
